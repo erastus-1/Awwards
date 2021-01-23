50a8ccf03af57ddb635683dd0045ff0c75680fae
erastus <erastuskariuki15@gmail.com>
2021-01-23 15:57:28 +0300

    added updateform

---
+++ wardapp/forms.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from .models import *
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['email']
+
+class UpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['user', 'bio', 'image']   
