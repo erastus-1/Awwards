cd9e7562b5ebc8027e02cf57bc258204812e5385
erastus <erastuskariuki15@gmail.com>
2021-01-23 11:21:52 +0300

    my base view

--- wardapp/views.py
+++ wardapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def home(request):
+
+    return render(request,'home.html',locals())
