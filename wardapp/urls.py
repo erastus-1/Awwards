19b5253655ebcbf8d0b5018ec688c17b04d412f3
erastus <erastuskariuki15@gmail.com>
2021-01-23 16:05:10 +0300

    added profile view and updateProfile urls

--- wardapp/urls.py
+++ wardapp/urls.py
@@ -5,7 +5,10 @@ from . import views
 
 urlpatterns=[
     url('^$',views.home,name = 'home'),
-    url('search/', views.search, name='search'),
+    url('profile/'views.profile_info, name='profile'),
+    url('update/'views.profile_update, name='update'),
+
 ]
+
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
