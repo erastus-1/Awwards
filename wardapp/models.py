4f3daa9e5b1e58ef6f6dc234f1d4fedf25b3ca25
erastus <erastuskariuki15@gmail.com>
2021-01-23 15:12:27 +0300

    added profile model

--- wardapp/models.py
+++ wardapp/models.py
@@ -1,3 +1,36 @@
+from cloudinary.models import CloudinaryField
+from djando.contrib.auth.models import User 
+from tinymce.models import HTMLFIELD
+from django.utils import timezone
 from django.db import models
+import cloudinary
 
 # Create your models here.
+class Profile(models.Model):
+    user = modles.OneToOneField(User, ond_delete=models.CASCADE, primary_key=True)
+    image = CloudinaryField('image', blank=True, null=True)
+    bio = HTMLField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+   
+   def __str__(self):
+        return str(self.bio)
+
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+
+    @classmethod
+    def update_bio(cls,id, bio):
+        update_profile = cls.objects.filter(id = id).update(bio = bio,)
+        return update_profile
+
+    @classmethod
+    def get_all_profiles(cls):
+        profile = Profile.objects.all()
+        return profile
+    
